af38e869010f2c53d28f6cf96b6c488e452e45ed
famousPete <pete.macaluso@generalassemb.ly>
2020-11-17 11:20:17 -0500

    [dinoController] show route!

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -9,4 +9,13 @@ dinoRouter.get('/', (req, res) => {
   res.render('dinosaurs/index', { dinos })
 })
 
+dinoRouter.get('/:id', (req, res) => {
+  const rawDinos = fs.readFileSync('./dinosaurs.json')
+  const dinos = JSON.parse(rawDinos)
+  const id = parseInt(req.params.id) - 1
+  const dino = dinos[id]
+
+  res.render('dinosaurs/show', { dino })
+})
+
 module.exports = dinoRouter
