8cb7919a04273d682c08b72789f901b190b967ad
famousPete <pete.macaluso@generalassemb.ly>
2020-11-17 10:51:22 -0500

    codealong up through dinos/index

---
+++ index.js
@@ -0,0 +1,22 @@
+// require stuff
+const app = require('express')()
+const layouts = require('express-ejs-layouts')
+
+const dinoRouter = require('./controllers/dinoController')
+
+// additional setup
+app.set('view engine', 'ejs')
+app.use(layouts)
+
+// our routes
+app.get('/', (req, res) => {
+  res.send('hello I am alive!')
+})
+
+app.use('/dinosaurs', dinoRouter)
+
+
+// listen!
+app.listen(8000, () => {
+  console.log('server started!')
+})
